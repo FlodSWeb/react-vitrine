8d5c472845b35ea2629b35d68bd0c45384bc41b6
Flods2020 <florian.desieter2020@campus-eni.fr>
2023-04-13 22:32:23 +0200

    Buttons ContactForm DynamicText Logo Project components

---
+++ src/components/ContactForm.js
@@ -0,0 +1,49 @@
+import React, { useRef } from "react";
+import { init } from "@emailjs/browser";
+init(process.env.ID);
+
+const ContactForm = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    const formMess = document.querySelector(".formMessage");
+
+    // A implementer via emailjs
+    // emailjs
+    //   .sendForm(
+    //     "service_ferfger",
+    //     "template_drzaekz",
+    //     form.current,
+    //     process.env.REACT_APP_ID
+    //   )
+    //   .then((res) => {
+    //     form.current.reset();
+    //     formMess.innerHTML = "<p class='success'>Message sent </p>";
+    //   });
+    // ............................
+  };
+  return (
+    <div className="form-container">
+      <h2>Contact us</h2>
+      <form ref={form} onSubmit={sendEmail} className="form-content">
+        <label>Name</label>
+        <input type="text" name="name" required autoComplete="off" id="name" />
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          required
+          autoComplete="off"
+          id="email"
+        />
+        <label>Message</label>
+        <textarea name="message" id="mess" />
+        <input type="submit" value="Send" className="hover button" />
+      </form>
+      <div className="formMessage"></div>
+    </div>
+  );
+};
+
+export default ContactForm;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,21 @@
 import React from "react";
+import Buttons from "../components/Buttons";
+import ContactForm from "../components/ContactForm";
+import Logo from "../components/Logo";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <main>
+      <Mouse />
+      <div className="contact">
+        <Navigation />
+        <Logo />
+        <ContactForm />
+        <Buttons left={"/project-4"} right={"/"} />
+      </div>
+    </main>
   );
 };
 
